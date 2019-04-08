e140ca6ea2f3de7534c0a75fafaf3d1e83c034c7
Julio Pinto Coelho <juliopcrj@gmail.com>
2019-04-07 21:29:21 -0300

    Doing stuff with nn

--- DM/neuralnet.R
+++ DM/neuralnet.R
@@ -11,14 +11,22 @@ scale_column = function(x){
 }
 
 data.norm = as.data.frame(lapply(data.norm, scale_column))
+amostra = 0.7 * nrow(data.norm)
+index = sample(seq_len(nrow(data.norm)), size=amostra)
 
-names(data.norm)
+data.train = data.norm[index,]
+data.test = data.norm[-index,]
 
 '%!in%' <- function(x,y)!('%in%'(x,y))
 
 nn <- neuralnet(p1_shoot + p1_horizontal + p1_vertical~p1_x + p1_y + p1_score + p2_x +
                   p2_y + p2_horizontal + p2_vertical + p2_shoot,
-                data.norm, hidden = 3)
+                data.train, hidden = 3)
+
+prev = compute(nn, data.test[,-c(4, 5, 6)])
 
 
-nrow(stats)
+prev$net.result
+
+nrow(prev$net.result)
+nrow(data.test)
